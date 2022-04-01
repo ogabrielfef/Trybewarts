56d8e27b8b3cf4d3c1d2f30868f4c74fcf6aab63
Marcus Vinicius <xxvi.paris@gmail.com>
2022-04-01 12:57:40 -0300

    adiciona requisito 18

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ function validateForm() {
     return window.alert('Email ou senha inválidos.')
 }
 
-document.getElementById('login').addEventListener('click', validateForm)
+document.getElementById('login').addEventListener('click', validateForm);
+
+document.getElementById("agreement").addEventListener("click", function(event){
+    if (document.getElementById('agreement').checked == true) {
+        document.getElementById("submit-btn").disabled = false;
+    }
+  });
